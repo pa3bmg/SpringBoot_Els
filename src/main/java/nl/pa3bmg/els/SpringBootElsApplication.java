c4981c8d5414188d0a71d60a7f3ebd2a8f1aab95
Ton Smink <pa3bmg@home.nl>
2018-02-05 16:43:18 +0100

    Eerste Commit

---
+++ src/main/java/nl/pa3bmg/els/SpringBootElsApplication.java
@@ -0,0 +1,12 @@
+package nl.pa3bmg.els;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElsApplication.class, args);
+	}
+}
